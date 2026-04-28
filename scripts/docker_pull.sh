dc8af29e27439b4df5089ddae7cbd8bd31ae96de
radu-matei1enciu <enciuradumatei@gmail.com>
2026-04-28 11:33:34 +0200

    Fix: Renamed scripts for starting and stopping the kind cluster

--- scripts/docker_pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-IMAGES=$(grep -rh 'image:' k8s/   | grep -v '#'   | awk '{print $2}'   | tr -d '"'   | sort -u)
-for IMAGE in $IMAGES; do
-  echo ">>> $IMAGE"
-  docker pull "$IMAGE"
-  kind load docker-image "$IMAGE" --name edcv
-done
